92135a500c77fd10b88a52eab2bbe0ce9b27373e
Benjamin HUBERT <benjamin@alpixel.fr>
2016-05-31 15:58:12 +0200

    Debugging the formtype reset button

--- src/Alpixel/Bundle/FormBundle/Subscribers/FormCookieSubscriber.php
+++ src/Alpixel/Bundle/FormBundle/Subscribers/FormCookieSubscriber.php
@@ -2,18 +2,25 @@
 
 namespace Alpixel\Bundle\FormBundle\Subscribers;
 
+use Doctrine\ORM\EntityManager;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Form;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\SubmitButton;
 use Symfony\Component\HttpFoundation\Session\Session;
 
 class FormCookieSubscriber implements EventSubscriberInterface
 {
+    protected $entityManager;
     protected $session;
 
-    public function __construct(Session $session)
+    public function __construct(Session $session, EntityManager $entityManager)
     {
+        $this->entityManager = $entityManager;
         $this->session = $session;
     }
 
@@ -33,22 +40,32 @@ class FormCookieSubscriber implements EventSubscriberInterface
 
         if ($this->session->has($formName)) {
             $filters = $this->session->get($formName);
-
             foreach ($filters as $field => &$value) {
                 if ($form->has($field)) {
                     $fieldConfig = $form->get($field)->getConfig();
                     $fieldType = $fieldConfig->getType()->getName();
+
                     switch ($fieldType) {
+                        case 'alpixel_entity_id':
+                            $entityManager = $fieldConfig->getOption('em');
+                            if ($entityManager === null) {
+                                $entityManager = $this->entityManager;
+                            }
+                            $className = $fieldConfig->getOption('class');
+                            $value = $entityManager
+                                ->getRepository($className)
+                                ->find($value);
+                            break;
                         case 'entity':
                             $entityManager = $fieldConfig->getOption('em');
                             $className = $fieldConfig->getOption('class');
                             $value = $entityManager
-                                        ->getRepository($className)
-                                        ->find($value);
-                        break;
+                                ->getRepository($className)
+                                ->find($value);
+                            break;
                         case 'checkbox':
-                            $value = (bool) $value;
-                        break;
+                            $value = (bool)$value;
+                            break;
                     }
                 }
             }
@@ -56,7 +73,7 @@ class FormCookieSubscriber implements EventSubscriberInterface
             $event->setData($filters);
         }
 
-        $form->add('reset', 'reset', [
+        $form->add('reset', SubmitType::class, [
             'label' => 'Réinitialiser le formulaire',
             'attr'  => [
                 'class' => 'btn btn-warning reset-cookie-form-action',
@@ -75,11 +92,11 @@ class FormCookieSubscriber implements EventSubscriberInterface
     public function onPostSubmit(FormEvent $event)
     {
         $form = $event->getForm();
-        if ($form->get('reset') instanceof Form) {
+
+        if ($form->get('reset') instanceof SubmitButton) {
             $reset = $form->get('reset');
             $formName = $this->getSessionName($form);
-
-            if ($reset->isSubmitted() === true && $this->session->has($formName) === true) {
+            if ($reset->isClicked() === true && $this->session->has($formName) === true) {
                 $this->session->remove($formName);
             }
         }
@@ -87,6 +104,6 @@ class FormCookieSubscriber implements EventSubscriberInterface
 
     private function getSessionName(Form $form)
     {
-        return 'filter_form_'.$form->getConfig()->getName();
+        return 'filter_form_' . $form->getConfig()->getName();
     }
 }
